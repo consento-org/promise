55feb8ad3b90077eb7c23423b9a398217271148c
Martin Heidegger <martin.heidegger@gmail.com>
2020-12-01 03:10:26 +0900

    refactor: cleaned up extPromise.

--- src/extPromise.ts
+++ src/extPromise.ts
@@ -1,19 +1,15 @@
-/* eslint-disable @typescript-eslint/method-signature-style */
-export interface IExtPromise<T> extends Promise<T> {
-  _resolve (data: T): void
-  _reject (error: Error): void
+export class ExtPromise <T> extends Promise <T> {
+  resolve!: (data: T) => void
+  reject!: (error: Error) => void
+  constructor () {
+    super((resolve, reject) => {
+      this.resolve = resolve
+      this.reject = reject
+    })
+  }
 }
 
-/* eslint @typescript-eslint/promise-function-async: "off" */
-export function extPromise <T> (): IExtPromise<T> {
-  let _resolve
-  let _reject
-  const result = new Promise<T>((resolve, reject) => {
-    _resolve = resolve
-    _reject = reject
-  }) as IExtPromise<T>
-  result._resolve = _resolve as unknown as (data: T) => void
-  result._reject = _reject as unknown as (error: Error) => void
-  // eslint-disable-next-line @typescript-eslint/return-await
-  return result
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+export function extPromise <T> (): ExtPromise<T> {
+  return new ExtPromise<T>()
 }
