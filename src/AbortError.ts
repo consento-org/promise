e928eb28f7bf7affe81b54b28de54a89318bac2e
Martin Heidegger <martin.heidegger@gmail.com>
2020-12-02 13:46:40 +0900

    fix: Aligning the AbortError code with NodeJS error.
    Signed-off-by: Martin Heidegger <martin.heidegger@gmail.com>

--- src/AbortError.ts
+++ src/AbortError.ts
@@ -1,9 +1,14 @@
 /**
  * To be thrown when a Signal is aborted.
+ *
+ * Equal to Node.JS's AbortError
+ *
+ * @see https://github.com/nodejs/node/blob/1ed72f67f5ea82b36b8589e447619e98c004fa12/lib/internal/errors.js#L735-L741
  */
 export class AbortError extends Error {
-  code = 'aborted'
+  code = 'ABORT_ERR'
+  name = 'AbortError'
   constructor () {
-    super('aborted')
+    super('The operation was aborted')
   }
 }
